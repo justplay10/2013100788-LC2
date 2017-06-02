9c92d953225eac3ea2baa55406f592778b009af6
Your Name <you@example.com>
2017-06-02 00:59:26 -0500

    LC3 Y LC4

--- 2013100788-ENT/Entities/LineaTelefonica.cs
+++ 2013100788-ENT/Entities/LineaTelefonica.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace _2013100788_ENT.Enum
+namespace _2013100788_ENT.Entities
 {
     class LineaTelefonica
     {
--- 2013100788-ENT/Properties/AssemblyInfo.cs
+++ 2013100788-ENT/Properties/AssemblyInfo.cs
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // El siguiente GUID sirve como id. de typelib si este proyecto se expone a COM.
-[assembly: Guid("3bc73e3d-60dc-401d-b5e6-a475c294e07b")]
+[assembly: Guid("0bc82b54-7db0-4d30-97ce-d495ab2955e6")]
 
 // La información de versión de un ensamblado consta de los cuatro valores siguientes:
 //
@@ -29,8 +29,8 @@ using System.Runtime.InteropServices;
 //      Número de compilación
 //      Revisión
 //
-// Puede especificar todos los valores o utilizar los números de compilación y de revisión predeterminados
-// mediante el carácter '*', como se muestra a continuación:
+// Puede especificar todos los valores o usar los números de compilación y de revisión predeterminados
+// mediante el carácter "*", como se muestra a continuación:
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
